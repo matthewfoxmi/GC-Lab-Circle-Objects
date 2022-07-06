967d359635d120917fab29b74dc8cce4e8ac9b2b
matthewfoxmi <matthewfoxmi@gmail.com>
2022-07-05 21:10:37 -0400

    Finished

---
+++ GC Lab Circle Objects/Circle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab_Circle_Objects
+{
+    internal class Circle
+    {
+        //properties
+        public double radius;
+        //public double area;
+        //public double circumference;
+
+
+        //constructor
+        public Circle(double _radius)
+        {
+            radius = _radius;
+
+        }
+
+        //methods
+
+        public double CalculateCircumference()
+        {
+            return 2 * (Math.PI * radius);
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCircumference = FormatNumber(CalculateCircumference());
+            return formattedCircumference; ;
+        }
+
+        public double CalculateArea()
+        {   
+            double area = Math.PI * radius * radius;
+            return area;
+                
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+
+
+        private string FormatNumber(double x)
+        {
+            //use for calculations
+            return Math.Round(x,2).ToString();
+        }
+    }
+}
---
+++ GC Lab Circle Objects/Program.cs
@@ -0,0 +1,79 @@
+﻿using GC_Lab_Circle_Objects;
+
+double radius = 0;
+string area = "";
+string circumference = "";
+
+bool goAgain = true;
+int circleCount = 0;
+
+Console.WriteLine("welcome to the circle tester");
+
+//Test for valid input from user
+while (goAgain)
+{
+    while (true)
+    {       
+    try
+        {
+            Console.WriteLine("enter a radius");
+            radius = double.Parse(Console.ReadLine());
+            if (radius < 0)
+            {
+                throw new Exception("The radius has to be positive.");
+            }
+            else
+            {
+                break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            continue;
+        }
+    }
+
+    //increment circle count if a valid number was entered
+    circleCount++;
+
+    //calculations
+
+    Circle myCircle = new Circle(radius);
+
+    area = myCircle.CalculateFormattedArea();
+    circumference = myCircle.CalculateFormattedCircumference();
+
+    Console.WriteLine($"Area: {area}\nCircumference: {circumference}");
+    
+    //continue loop    
+
+    while (true)
+    {
+        Console.WriteLine("Would you like to continue? Enter y/n");
+        string input = Console.ReadLine().ToLower().Trim();
+
+        if (input == "n")
+        {   //end program
+            Console.WriteLine("Goodbye!");
+            Console.WriteLine($"Number of circles calculated: {circleCount}");
+            goAgain = false;
+            break;
+        }
+        else if (input == "y")
+        {
+            //loop to beginning
+            break;
+        }
+        else
+        {   //ask user to enter y/n
+            Console.WriteLine("Enter y/n");
+            
+        }
+    }
+}
+
+
+
+
+
